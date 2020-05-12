3db6a4b48649b5fe3947db47ad8a55483a5af058
Shane R. Spencer <305301+whardier@users.noreply.github.com>
2020-05-12 05:26:27 +0000

    init

---
+++ http_fdw/__init__.py
@@ -0,0 +1,1 @@
+from .http_fwd import HttpFDW
--- http_fdw/http_fwd.py
+++ http_fdw/http_fwd.py
@@ -30,6 +30,17 @@
 
 """Multicorn based HTTP client PostgreSQL foreign data wrapper"""
 
+import json
+
+from datetime import datetime
+from traceback import extract_stack
+from urllib.parse import quote
+from urllib.parse import quote_plus
+from urllib.parse import urlencode
+from urllib.parse import urlparse
+from urllib.parse import urlunparse
+from urllib.parse import parse_qsl
+from urllib.request import Request
 from urllib.request import urlopen
 
 from multicorn import ColumnDefinition
@@ -41,22 +52,105 @@ from multicorn.utils import WARNING
 from multicorn.utils import log_to_postgres
 
 
-# ╻ ╻╺┳╸╺┳╸┏━┓┏━╸╺┳┓╻ ╻
-# ┣━┫ ┃  ┃ ┣━┛┣╸  ┃┃┃╻┃
-# ╹ ╹ ╹  ╹ ╹  ╹  ╺┻┛┗┻┛
+def true_or_false(bool_str):
+    if bool_str == "true":
+        return True
+    return False
+
+
+def post(url, body, body_type, encoding):
+
+    headers = {}
+
+    if body_type == "form":
+        data = urlencode(body).encode(encoding)
+        headers["Content-Type"] = "application/x-www-form-urlencoded"
+    elif body_type == "json":
+        data = json.dumps(body).encode(encoding)
+        headers["Content-Type"] = "application/json"
+
+    request = Request(url=url, data=data, headers=headers, method="POST")
+
+    response = urlopen(request)
+
+
+def get(url):
+    request = Request(url=url, method="GET")
+    response = urlopen(request)
+
+
+def url_append_data_as_query(url, query):
+    parsed_url = urlparse(url)
+    qsl = parse_qsl(parsed_url.query)
+    qsl.extend((key, val) for (key, val) in query.items())
+    parsed_url = parsed_url._replace(query=urlencode(qsl))
+    return urlunparse(parsed_url)
+
+
+def mapping_urlencode_variants(obj, encoding="utf-8"):
+    mapping_safe = {quote(f"{key}"): quote_plus(str(val)) for (key, val) in obj.items()}
+    mapping_unsafe = {
+        quote(f"{key}_unsafe"): quote(str(val)) for (key, val) in obj.items()
+    }
+    return {**mapping_safe, **mapping_unsafe}
+
+
 class HttpFDW(ForeignDataWrapper):
     def __init__(self, fdw_options, fdw_columns):
         super(HttpFDW, self).__init__(fdw_options, fdw_columns)
 
-        self.url_template = fdw_options["url"]
-        # log_to_postgres(repr((fdw_options, fdw_columns)), level=DEBUG)
+        self.url_initial = fdw_options["url"]
+        self.execute_url_initial = fdw_options.get("execute_url", self.url_initial)
+        self.insert_url_initial = fdw_options.get("insert_url", self.url_initial)
 
-    def execute(self, quals, columns):
-        log_to_postgres(repr((quals, columns)), level=DEBUG)
+        self.url_prerendered = self.url_initial.format(**vars(self))
+        self.execute_url_prerendered = self.execute_url_initial.format(**vars(self))
+        self.insert_url_prerendered = self.insert_url_initial.format(**vars(self))
 
-    def insert(self, values):
-        log_to_postgres(repr((values,)), level=DEBUG)
+        self.method = fdw_options.get("method", "POST")
+        self.execute_method = fdw_options.get("execute_method", self.method)
+        self.insert_method = fdw_options.get("insert_method", self.method)
+
+        self.body_type = fdw_options.get("body_type", "form")
+
+        self.encoding = fdw_options.get("encoding", "utf-8")
+
+        data_as_query = fdw_options.get("data_as_query", "true")
+        self.data_as_query = true_or_false(data_as_query)
+
+        self.fdw_options = fdw_options
+        self.fdw_columns = fdw_columns
 
     @property
     def rowid_column(self):
         return "fdw_rowid"
+
+    def execute(self, quals, columns):
+        execute_url_rendered = self.execute_url_prerendered.format(
+            http_fdw_function="execute",
+            http_fdw_method=self.execute_method,
+            **mapping_urlencode_variants(quals),
+        )
+
+        if self.data_as_query:
+            insert_url_rendered = url_append_data_as_query(insert_url_rendered, quals)
+
+        if self.method == "POST":
+            post(insert_url_rendered, quals, self.body_type, self.encoding)
+        elif self.method == "GET":
+            get(insert_url_rendered)
+
+    def insert(self, values):
+        insert_url_rendered = self.insert_url_prerendered.format(
+            http_fdw_function="insert",
+            http_fdw_method=self.insert_method,
+            **mapping_urlencode_variants(values),
+        )
+
+        if self.data_as_query:
+            insert_url_rendered = url_append_data_as_query(insert_url_rendered, values)
+
+        if self.method == "POST":
+            post(insert_url_rendered, values, self.body_type, self.encoding)
+        elif self.method == "GET":
+            get(insert_url_rendered)
